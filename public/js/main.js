2ea570a2885eaded36acdc95ad7b97ddbbebdd41
komed3 <pk97@outlook.de>
2026-03-26 13:45:43 +0100

    add imports

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
+import { docCtx, setupCanvas, clearRegions, resetZoom, cropRegionToBlob, getRegions, getDocumentBlob } from './canvas.js';
 import { initI18n, t } from './i18n.js';
+import { processOcr } from './ocr.js';
+import { loadPdf } from './pdf.js';
 import { showLoader, hideLoader, showStep, showToast, calcStats } from './utils.js';
 
 await initI18n();

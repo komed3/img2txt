3c2415fc710b1b53bd2319fc56934e6e0c7ff82a
komed3 <pk97@outlook.de>
2026-03-27 18:15:26 +0100

    init canvas workspace

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -36,4 +36,51 @@ export class CanvasWorkspace {
         this.init();
     }
 
+    init () {
+        $( 'zoomInBtn' ).onclick = () => this.zoomBtn( 0.2 );
+        $( 'zoomOutBtn' ).onclick = () => this.zoomBtn( -0.2 );
+        $( 'zoomResetBtn' ).onclick = () => this.resetZoom();
+
+        this.workspace.onwheel = ( e ) => this.handleWheel( e );
+        this.workspace.onmousedown = ( e ) => this.onMouseDown( e );
+        window.onmousemove = ( e ) => this.onMouseMove( e );
+        window.onmouseup = ( e ) => this.onMouseUp( e );
+        window.onresize = () => this.resetZoom();
+
+        this.overCanvas.onkeydown = ( e ) => this.handleKeys( e );
+
+        // Block middle click autoscroll
+        document.addEventListener( 'mousedown', ( e ) => {
+            if ( e.button === 1 ) e.preventDefault(); 
+        }, { passive: false } );
+    }
+
+    getRegions () {
+        return this.regions;
+    }
+
+    setupCanvas ( width, height ) {
+        this.currentImageWidth = width;
+        this.currentImageHeight = height;
+
+        // Protection against hardware canvas limits (e.g. 20.000px)
+        this.renderScale = 1.0;
+        if ( width > this.MAX_CANVAS_DIM || height > this.MAX_CANVAS_DIM ) {
+            this.renderScale = Math.min( this.MAX_CANVAS_DIM / width, this.MAX_CANVAS_DIM / height );
+        }
+
+        const virtualW = width * this.renderScale;
+        const virtualH = height * this.renderScale;
+
+        this.docCanvas.width = virtualW;
+        this.docCanvas.height = virtualH;
+
+        this.overCanvas.width = virtualW + this.OVERLAY_PAD * 2;
+        this.overCanvas.height = virtualH + this.OVERLAY_PAD * 2;
+        this.overCanvas.style.left = -this.OVERLAY_PAD + "px";
+        this.overCanvas.style.top = -this.OVERLAY_PAD + "px";
+
+        this.resetZoom();
+    }
+
 }
